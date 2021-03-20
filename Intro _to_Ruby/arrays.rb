4f28a7719b01c5253e683b2242c9457a17d58081
Carlos Lopez <lopezmoc@oregonstate.edu>
2021-03-19 23:23:01 -0700

    :heavy_plus_sign: Include 2D array section

--- Intro _to_Ruby/arrays.rb
+++ Intro _to_Ruby/arrays.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 # Write a method doubler(numbers) that takes an array of numbers and returns a new array
 # where every element of the original array is multiplied by 2.
 
@@ -352,3 +354,135 @@ puts is_valid_email("jdoe@g@mail.com")  # => false
 puts is_valid_email("jdoe42@gmail.com") # => false
 puts is_valid_email("jdoegmail.com")    # => false
 puts is_valid_email("az@email")         # => false
+
+# 2D Arrays
+
+# Write a method combinations that takes in an array of unique elements, the method should return a 2D array representing all possible combinations of 2 elements of the array.
+def combinations(arr)
+  	solution = []
+	arr.each do |i|
+    arr.each do |j|
+      if (j > i)
+        solution.push([i,j])            	
+      end
+    end
+  end
+  return solution
+end
+
+print combinations(["a", "b", "c"]); # => [ [ "a", "b" ], [ "a", "c" ], [ "b", "c" ] ]
+puts
+
+print combinations([0, 1, 2, 3]); # => [ [ 0, 1 ], [ 0, 2 ], [ 0, 3 ], [ 1, 2 ], [ 1, 3 ], [ 2, 3 ] ]
+puts
+
+# Write a method opposite_count that takes in an array of unique numbers. The method should return the number of pairs of elements that sum to 0.
+def opposite_count(nums)
+	count = 0
+  nums.each do |i|
+    nums.each do |j|
+      if (j > i && i + j == 0)
+        count += 1
+      end
+    end
+  end
+  return count
+end
+
+puts opposite_count([ 2, 5, 11, -5, -2, 7 ]) # => 2
+puts opposite_count([ 21, -23, 24 -12, 23 ]) # => 1
+
+# Write a method two_d_Sum that takes in a two dimensional array and returns the sum of all elements in the array.
+def two_d_sum(arr)
+	sum = 0
+  arr.each do |subArray|
+    subArray.each do |i|
+      sum += i
+    end
+  end
+  return sum
+end
+
+array_1 = [
+  [4, 5],
+  [1, 3, 7, 1]
+]
+puts two_d_sum(array_1)    # => 21
+
+array_2 = [
+  [3, 3],
+  [2],
+  [2, 5]
+]
+puts two_d_sum(array_2)    # => 15
+
+# Write a method two_d_translate that takes in a 2 dimensional array and translates it into a 1 dimensional array. You can assume that the inner arrays always have 2 elements. See the examples.
+
+def two_d_translate(arr)
+  result = []
+	arr.each do |subArray|
+    subArray[1].times do 
+      result.push(subArray[0])
+    end
+  end
+  return result
+end
+
+arr_1 = [
+  ['boot', 3],
+  ['camp', 2],
+  ['program', 0]
+]
+
+print two_d_translate(arr_1) # => [ 'boot', 'boot', 'boot', 'camp', 'camp' ]
+puts
+
+arr_2 = [
+  ['red', 1],
+  ['blue', 4]
+]
+
+print two_d_translate(arr_2) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
+puts
+
+# Write a method array_translate that takes in an array whose elements alternate between words and numbers. The method should return a string where each word is repeated the number of times that immediately follows in the array.
+
+def array_translate(array)
+  translated = ""
+  i = 0
+  while i < array.length
+  	array[i+1].times do
+    	translated += array[i]
+    end
+    i += 2
+  end
+  return translated
+end
+
+print array_translate(["Cat", 2, "Dog", 3, "Mouse", 1]); # => "CatCatDogDogDogMouse"
+puts
+
+print array_translate(["red", 3, "blue", 1]); # => "redredredblue"
+puts
+
+# Write a method pig_latin_word that takes in a word string and translates the word into pig latin.
+# Pig latin translation uses the following rules:
+# - for words that start with a vowel, add 'yay' to the end
+# - for words that start with a nonvowel, move all letters before the first vowel to the end of the word and add 'ay'
+
+def pig_latin_word(word)
+	vowels = "a,e,i,o,u"
+  	if ( vowels.include?(word[0]) )
+      return word + "yay"
+    end
+  	for i in (0...word.length)
+      if (vowels.include?(word[i]))
+        return word[i..-1] + word[0...i] + "ay"
+      end
+    end
+end
+
+puts pig_latin_word("apple")   # => "appleyay"
+puts pig_latin_word("eat")     # => "eatyay"
+puts pig_latin_word("banana")  # => "ananabay"
+puts pig_latin_word("trash")   # => "ashtray"
